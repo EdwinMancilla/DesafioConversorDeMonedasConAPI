6de9dc3f6f096cd0f0f312a876a6dbe43a84bfa6
Edwin Mancilla <edwinhuevo10@gmail.com>
2025-05-22 14:09:31 -0600

    Tipos de Cambio y ClientesParaSolicitar

---
+++ src/ClienteParaSolicitudes.java
@@ -0,0 +1,35 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ClienteParaSolicitudes {
+
+    public TiposDeCambio tiposDeCambio(int ARS,
+                                       int BOB,
+                                       int BRL,
+                                       int CLP,
+                                       int COP,
+                                       int USD) {
+        URI direccion = URI.create("https://v6.exchangerate-api.com/v6/6e96f732f4112e9ac8008af8/latest/" );
+
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return  new Gson().fromJson(response.body(), TiposDeCambio.class);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ src/TiposDeCambio.java
@@ -0,0 +1,7 @@
+public record TiposDeCambio(int ARS,
+                            int BOB,
+                            int BRL,
+                            int CLP,
+                            int COP,
+                            int USD) {
+}
